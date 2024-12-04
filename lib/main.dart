f8348e6ee2b3c49b7ce1a2168bca082319e0cd7c
Omar Abdullah <omarabdulla1>
2024-12-04 16:21:02 +0200

    - Just Upadted the app title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'RDivxe Browser',
+      title: 'Rdivxe Browser',
       home: BrowserHome(),
     );
   }
