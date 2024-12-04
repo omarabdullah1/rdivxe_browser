01884847749f3a90c19af24b11664f674d172363
Omar Abdullah <omarabdulla1>
2024-12-04 16:01:37 +0200

    - Added Splash screen for android below above 12 - Added splash logo for android below above 12

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'RDivxe Browser',
       home: BrowserHome(),
     );
   }
